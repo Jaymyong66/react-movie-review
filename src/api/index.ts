9214f64f65ee5b0dbaa9b07762989981e51fe013
jayming66 <mjw8941@gmail.com>
2024-06-11 14:47:47 +0900

    feat: API 응답 타입 맞추기

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,20 @@ interface FetchMoviesParams {
   page?: number;
 }
 
+const GET_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
+  },
+};
+
 export const fetchMovies = async ({ page }: FetchMoviesParams) => {
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/popular?language=ko&page=${page}`,
+    GET_OPTIONS,
   );
-  const { results } = await response.json();
+  const data = await response.json();
 
-  return results;
+  return data;
 };
