ce3c879d4cd0f4730f68957a61a1b99d50a8ab69
jayming66 <mjw8941@gmail.com>
2024-06-11 14:19:15 +0900

    feat(useMovies.test.tsx): 테스트 통과 시키기

--- src/__tests__/useMovies.test.tsx
+++ src/__tests__/useMovies.test.tsx
@@ -1,24 +1,33 @@
-import { renderHook, waitFor } from '@testing-library/react';
-import { act } from 'react';
+import useMovies from '@/hooks/useMovies';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { act, renderHook, waitFor } from '@testing-library/react';
+
+const queryClient = new QueryClient();
+
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
 
 describe('useMovies 훅 테스트', () => {
   it('인기 있는 영화 목록의 1 페이지를 가져온다', async () => {
-    const { result } = renderHook(() => useMovies());
+    const { result } = renderHook(() => useMovies(), { wrapper });
 
     await waitFor(() => {
-      expect(result.current.data).toHaveLength(20);
+      expect(result.current.isMoviesSuccess).toBe(true);
+      expect(result.current.movies).toHaveLength(20);
     });
   });
 
   it('인기 있는 영화 목록의 2 페이지를 가져온다', () => {
-    const { result } = renderHook(() => useMovies());
+    const { result } = renderHook(() => useMovies(), { wrapper });
 
     act(() => {
       result.current.fetchNextPage();
     });
 
     waitFor(() => {
-      expect(result.current.data).toHaveLength(40);
+      expect(result.current.isMoviesSuccess).toBe(true);
+      expect(result.current.movies).toHaveLength(40);
     });
   });
 });
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,17 @@
-import { http, HttpResponse } from "msw";
+import { http, HttpResponse } from 'msw';
+import movies from './movies.json';
 
 export const handlers = [
-  http.get("http://example.com", () => {
-    return HttpResponse.json();
+  http.get('https://api.themoviedb.org/3/movie/popular', ({ request }) => {
+    const url = new URL(request.url);
+    const page = Number(url.searchParams.get('page'));
+
+    const startIndex = (page - 1) * 20;
+    const endIndex = page * 20;
+
+    const last = !movies.results[endIndex + 1];
+    const formattedMovies = movies.results.slice(startIndex, endIndex);
+
+    return HttpResponse.json({ page, last, results: [...formattedMovies] });
   }),
 ];
